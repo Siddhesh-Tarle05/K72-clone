651824e5b949b69997d14e75d1599d3425c14422
Siddhesh-Tarle05 <tarlesiddhesh@gmail.com>
2026-02-14 22:28:57 +0530

    updated vid link

--- src/components/home/Videodiv.jsx
+++ src/components/home/Videodiv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Videodiv = () => {
   return (
     <div className='Videodiv'>
-      <video autoPlay muted loop src="src\assets\vid.mp4" className='vid'></video>
+      <video autoPlay muted loop src="/vid.mp4" className='vid'></video>
     </div>
   )
 }
